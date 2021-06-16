157b9e8c748959a401f31f6d4bcb741283146cfd
Tom Bechar <tomchick30@gmail.com>
2021-06-16 17:31:15 +0300

    added locs table

--- js/main-controller.js
+++ js/main-controller.js
@@ -34,8 +34,22 @@ function onAddMarker() {
 function onGetLocs() {
   locService.getLocs().then((locs) => {
     console.log('Locations:', locs);
-    document.querySelector('.locs').innerText = JSON.stringify(locs);
-  });
+    // document.querySelector('.locs').innerText = JSON.stringify(locs);
+    var strHtml = locs.map((loc) =>{
+      return `<tr>
+      <td>${loc.name}</td>
+      <td>${loc.lat}</td>
+      <td>${loc.lng}</td>
+      <td><button class="go-btn" data-lat="${loc.lat}" data-lng="${loc.lng}">Go</button></td>
+  </tr>`
+    }).join('')
+    document.querySelector('.locs').innerHTML= strHtml;
+    const elbtns = document.querySelectorAll('.go-btn');
+    elbtns.forEach((btn) => {
+      console.log(btn.dataset.lat , btn.dataset.lng);
+      btn.onclick = onGoTo(btn.dataset.lat,btn.dataset.lng);
+    })
+  })
 }
 
 function onGetUserPos() {
@@ -51,12 +65,19 @@ function onGetUserPos() {
       console.log('err!!!', err);
     });
 }
+
+function onGoTo(lat,lng){
+  mapService.panTo(lat,lng);
+}
+
 function onPanTo() {
   console.log('Panning the Map');
-  var address = document.querySelector('.search-input').value
+  var address = document.querySelector('.search-input').value;
   locService.getCoords(address).then((res) => {
     mapService.panTo(res.lat,res.lng)
   });
+  document.querySelector('.search-input').value = '';
   // mapService.panTo(coords.lat,coords.lng);
 }
+
 
--- js/services/geocode-Service.js
+++ js/services/geocode-Service.js
@@ -1,15 +1,15 @@
 'use strict';
+
+import { storageService } from './storage-Service.js'
+
 export const locService = {
     getLocs,
     panTo,
-    getCoords
+    getCoords,
 }
 
+const locs = storageService.load('locationsDB') || [];
 
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
 
 function getLocs() {
     return new Promise((resolve, reject) => {
@@ -19,25 +19,38 @@ function getLocs() {
     });
 }
 
-function getCoords(address){
+function getCoords(address) {
+    if (findIdxByName(address) >= 0) return Promise.resolve(locs[findIdxByName(address)]);
+
+    console.log('Getting data from server');
     const API_KEY = 'AIzaSyBPyiw6Z3xEf5KBfpiCwH3zOBboChLgf8A';
     return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_KEY}`)
-    .then((res) => res.data)
-    .then((res) => res.results[0])
-    .then((res) => res.geometry)
-    .then((res) => res.viewport)
-    .then((res) => res.northeast)
-    .then((res) => {
-        return {
-            lat : res.lat,
-            lng : res.lng
-        }
-    })
+        .then((res) => res.data)
+        .then((res) => res.results[0])
+        .then((res) => res.geometry)
+        .then((res) => res.viewport)
+        .then((res) => res.northeast)
+        .then((res) => {
+            const loc = {
+                name: address,
+                lat: res.lat,
+                lng: res.lng,
+                createdAt: Date.now()
+            }
+            locs.push(loc);
+            storageService.save('locationsDB', locs);
+            return loc
+        })
 }
+
 
 
 function panTo(lat, lng) {
     var laLatLng = new google.maps.LatLng(lat, lng);
     gMap.panTo(laLatLng);
-  }
-  
+}
+
+
+function findIdxByName(name){
+   return locs.findIndex((location) => location.name === name)
+}
